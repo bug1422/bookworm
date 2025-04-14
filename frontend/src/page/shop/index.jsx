dcf8c248be5713394560e4034fb67daa2d3f492a
Phạm Quang Thái <87228468+bug1422@users.noreply.github.com>
2025-04-14 17:53:14 +0700

    Add Shop Page UI (#1)
    * fe: add filter section
    
    * add book list  section

--- frontend/src/page/shop/index.jsx
+++ frontend/src/page/shop/index.jsx
@@ -1,8 +1,19 @@
+import FilterSection from "./filterSection";
+import ListSection from "./listSection";
+import { QueryProvider } from "./usequeryContext";
+
 const ShopPage = () => {
-    return(
-<></>
+  return (
+    <QueryProvider>
+      <div>
+        <div className="py-8 mb-12 border-b-2 border-gray-200 xl:text-xl font-bold">Books <span className="text-base font-normal text-gray-300">(Filtered by Category #1)</span></div>
+      </div>
+      <div className="mb-16 grid grid-cols-[20%_auto] gap-4">
+        <FilterSection />
+        <ListSection />
+      </div>
+    </QueryProvider>
+  );
+};
 
-    )
-}
-
-export default ShopPage
+export default ShopPage;
