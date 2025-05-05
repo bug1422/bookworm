4466931be18b38fb21372168ea43a25a27d22d7d
bug1422 <thaiphamwang@gmail.com>
2025-05-05 09:53:45 +0700

    change order validation to return 409 on conflict cart

--- backend/app/models/exception.py
+++ backend/app/models/exception.py
@@ -9,10 +9,11 @@ class NotFoundException(Exception):
 class OrderValidationException(Exception):
     def __init__(self, order_output: OrderValidateOutput):
         super().__init__()
-        self.exception_details = order_output.exception_details
-        self.validated_items: list[OrderItemValidateOutput] = order_output.validated_items
+        self.order_output = order_output
+    def get_order_output(self):
+        return self.order_output
 
     def get_detail_string(self):
-        all_errors = ["cart:"+exception for exception in self.exception_details] + \
-            [f"{item.book_id}:"+exception for item in self.validated_items for exception in item.exception_details]
+        all_errors = ["cart:"+exception for exception in self.order_output.exception_details] + \
+            [f"{item.book_id}:"+exception for item in self.order_output.validated_items for exception in item.exception_details]
         return "|".join(filter(None, all_errors))
--- backend/app/services/order.py
+++ backend/app/services/order.py
@@ -6,6 +6,7 @@ from app.models.order import Order, OrderValidateOutput
 from app.models.order_item import OrderItemValidateOutput, OrderItemValidateInput, OrderItemCheckoutInput
 from app.models.exception import OrderValidationException
 
+
 class OrderService:
     def __init__(
         self,
@@ -18,10 +19,9 @@ class OrderService:
         self.item_service = item_service
 
     @res_wrapper
-    def add_order(self, user_id: int, order_items: list[OrderItemCheckoutInput]) -> Order:
-        user_res = self.user_service.get_user_info(user_id)
-        if not user_res.is_success:
-            raise user_res.exception
+    def validate_order(
+        self, order_items: list[OrderItemValidateInput]
+    ) -> OrderValidateOutput:
         validated_items: list[OrderItemValidateOutput] = []
         is_valid: bool = True
         for item in order_items:
@@ -31,14 +31,28 @@ class OrderService:
             validated_items.append(validated_item)
         exception_details = []
         self.__validate_unique_items(order_items, exception_details)
+        order_output = OrderValidateOutput(
+            exception_details=exception_details, validated_items=validated_items)
         if not is_valid or exception_details:
-            raise OrderValidationException(order_output=OrderValidateOutput(exception_details=exception_details,validated_items=validated_items))
+            raise OrderValidationException(order_output)
+        return order_output
+    
+    @res_wrapper
+    def add_order(self, user_id: int, order_items: list[OrderItemCheckoutInput]) -> Order:
+        user_res = self.user_service.get_user_info(user_id)
+        if not user_res.is_success:
+            raise user_res.exception
+        validated_order_res = self.validate_order(order_items)
+        if not validated_order_res.is_success:
+            raise validated_order_res.exception
+        validated_items = validated_order_res.result.validated_items
         total_price = sum([item.total_price for item in validated_items])
-        order = Order(user_id = user_id, order_amount=total_price)
+        order = Order(user_id=user_id, order_amount=total_price)
         self.repository.add(order)
         self.repository.flush()
         self.repository.refresh(order)
-        add_items_res = self.item_service.add_items(order_id=order.id,items=validated_items)
+        add_items_res = self.item_service.add_items(
+            order_id=order.id, items=validated_items)
         if not add_items_res.is_success:
             # rollback
             self.repository.rollback()
@@ -47,17 +61,6 @@ class OrderService:
         self.repository.refresh(order)
         return order
 
-    @res_wrapper
-    def validate_order(
-        self, order_items: list[OrderItemValidateInput]
-    ) -> OrderValidateOutput:
-        exception_details = []
-        self.__validate_unique_items(order_items, exception_details)
-        validated_items = [
-            self.item_service.validate_item(item)
-            for item in order_items
-        ]
-        return OrderValidateOutput(exception_details=exception_details, validated_items=validated_items)
 
     def __validate_unique_items(self, order_items: list[OrderItemValidateInput], exception_details: list[str]):
         if len(order_items) != len(set([item.book_id for item in order_items])):
